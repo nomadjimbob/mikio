eeb03b3ef6ec3ac14f961a89a7c80d579eaeb3b0
James Collins <james.collins@outlook.com.au>
2020-06-15 15:40:08 +1000

    added hide site icon option

--- conf/default.php
+++ conf/default.php
@@ -7,16 +7,26 @@
 $conf['includeFontAwesome'] = 1;
 $conf['useFontAwesome']     = 1;
 $conf['useTheme']           = '';
-$conf['navbarMenuStyle']    = 'icon and text';
+$conf['navbarIconsText']    = 'icons';
+$conf['navbarUseDropdown']  = 0;
 $conf['navbarClasses']      = 'navbar-dark navbar-expand-lg';
 $conf['navbarMenuPosition'] = 'right';
-$conf['navbarMenuClasses']  = '';
+$conf['navbarSearchPosition'] = 'left';
+$conf['navbarCustomPages']  = '';
+$conf['navbarGuestHide']    = 1;
+$conf['navbarHideImage']    = 0;
 $conf['useHeroTitle']       = 1;
 $conf['breadcrumbsLoc']     = 'page';
-$cont['tocfullheight']      = 0;
+$conf['tocfullheight']      = 0;
+$conf['hidePageTools']      = 0;
+$conf['hidePageToolsFooter'] = 0;
+$conf['showCustomPagesInNavbar'] = 1;
+$conf['showCustomPagesInFooter'] = 1;
+$conf['showSearchInFooter'] = 1;
+$conf['showSearchInSidebar'] = 'none';
 
 
-$conf['discussionPage']   = 'discussion:@ID@';
-$conf['userPage']         = 'user:@USER@:';
-$conf['hideTools']        = 0;
-$conf['navbar']           = '';
+// $conf['discussionPage']   = 'discussion:@ID@';
+// $conf['userPage']         = 'user:@USER@:';
+// $conf['hideTools']        = 0;
+// $conf['navbar']           = '';
