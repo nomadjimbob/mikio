ee85002b641b75ac018a4eb2a8270975e82ad5db
James Collins <james.collins@outlook.com.au>
2020-07-15 07:03:27 +1000

    Fixed missing header failure #5
    Remove require tpl_functions.php as already included at this point

--- mediamanager.php
+++ mediamanager.php
@@ -1,22 +1,18 @@
 <?php
 /**
- * DokuWiki Mikio Template Icons
+ * DokuWiki Media Manager Popup
  *
- * @link    http://dokuwiki.org/template:mikio
- * @author  James Collins <james.collins@outlook.com.au>
- * @license GPLv2 (http://www.gnu.org/licenses/gpl-2.0.html)
- * 
- * Based on DokuWiki Theme Template
+ * @author   Andreas Gohr <andi@splitbrain.org>
+ * @license  GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
+// must be run from within DokuWiki
 if (!defined('DOKU_INC')) die();
-@require_once(dirname(__FILE__).'/tpl_functions.php'); /* include hook for template functions */
 header('X-UA-Compatible: IE=edge,chrome=1');
 
 ?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>"
-  lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction'] ?>" class="popup no-js">
+<html lang="<?php echo $conf['lang']?>" dir="<?php echo $lang['direction'] ?>" class="popup no-js">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="utf-8" />
     <title>
         <?php echo hsc($lang['mediaselect'])?>
         [<?php echo strip_tags($conf['title'])?>]
@@ -26,19 +22,10 @@ header('X-UA-Compatible: IE=edge,chrome=1');
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <?php echo tpl_favicon(array('favicon', 'mobile')) ?>
     <?php tpl_includeFile('meta.html') ?>
-    
-    <?php
-        $baseDir = tpl_basedir();
-        echo '<link type="text/css" rel="stylesheet" href="' . $baseDir . 'css.php?css=assets/mikio.less">';
-        echo '<script type="text/javascript" src="' . $baseDir . 'assets/mikio.js"></script>';
-    ?>
-
-
 </head>
 
 <body>
-    <!--[if IE 8 ]><div id="IE8"><![endif]-->
-    <div id="media__manager" class="<?php echo tpl_classes(); ?>">
+    <div id="media__manager" class="dokuwiki">
         <?php html_msgarea() ?>
         <div id="mediamgr__aside"><div class="pad">
             <h1><?php echo hsc($lang['mediaselect'])?></h1>
@@ -53,6 +40,5 @@ header('X-UA-Compatible: IE=edge,chrome=1');
             <?php tpl_mediaContent() ?>
         </div></div>
     </div>
-    <!--[if lte IE 8 ]></div><![endif]-->
 </body>
 </html>
