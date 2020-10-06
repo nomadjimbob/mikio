7192a412fef0e4e54732a69517572607642c19dc
James Collins <james.collins@outlook.com.au>
2020-10-06 11:34:37 +1000

    Added missing language definitions

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -26,81 +26,87 @@ $lang['config_license']                     = 'License';
 $lang['config_acl']                         = 'Permissions';
 
 /* Style.ini */
-$lang['__font_size__']                      = 'The base font size (can be any size unit: %, px, em, rem)';
+$lang['__link__']                           = 'The font color of links';
+$lang['__line_height__']                    = 'The base font line height';
+
+$lang['__font_size__']                      = 'The base font size';
 $lang['__font_family__']                    = 'The base font family (fonts containing spaces must be in quotes. Multiple fonts can be seperated with commas)';
-$lang['__admin_background_color__']         = 'Background color of admin pages (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__admin_background_color__']         = 'Background color of admin pages';
 
 $lang['__code_font_family__']               = 'The font family for code blocks (fonts containing spaces must be in quotes. Multiple fonts can be seperated with commas)';
-$lang['__code_font_size__']                 = 'The font size for code blocks (can be any size unit: %, px, em, rem)';
-$lang['__code_text_color__']                = 'The font color for code blocks (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__code_background_color__']          = 'The background color for code blocks (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__code_font_size__']                 = 'The font size for code blocks';
+$lang['__code_text_color__']                = 'The font color for code blocks';
+$lang['__code_background_color__']          = 'The background color for code blocks';
+$lang['__code_line_height__']               = 'The font line height for code blocks';
+$lang['__code_word_wrap__']                 = 'The word wrap style for code blocks';
+$lang['__code_border_color__']              = 'The border color for code blocks';
 
-$lang['__control_text_color__']             = 'The font color for controls (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__control_border_color__']           = 'The border color for controls (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__control_background_color__']       = 'The background color for controls (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__input_border_color__']             = 'The border color for controls (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__control_text_color__']             = 'The font color for controls';
+$lang['__control_border_color__']           = 'The border color for controls';
+$lang['__control_background_color__']       = 'The background color for controls';
+$lang['__input_border_color__']             = 'The border color for controls';
 
-$lang['__navbar_background_color__']        = 'The background color for the menu (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__navbar_border_color__']            = 'The border color for the menu (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__navbar_brand_text_color__']        = 'The font color for the site title (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__navbar_brand_tagline_color__']     = 'The font color for the site tagline (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__navbar_background_color__']        = 'The background color for the menu';
+$lang['__navbar_border_color__']            = 'The border color for the menu';
+$lang['__navbar_brand_text_color__']        = 'The font color for the site title';
+$lang['__navbar_brand_tagline_color__']     = 'The font color for the site tagline';
 $lang['__navbar_content_justify__']         = 'The content justification of the site menu (can be flex-start, flex-end, center, space-between or space-around)';
 
-$lang['__subnavbar_text_color__']           = 'The font color for sub menu text (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__subnavbar_link_color__']           = 'The font color for sub menu links (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__subnavbar_link_hover_color__']     = 'The font color when the cursor hovers over sub menu links (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__subnavbar_background_color__']     = 'The background color for the sub menu (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__subnavbar_border_color__']         = 'The border color for the sub menu (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__subnavbar_text_color__']           = 'The font color for sub menu text';
+$lang['__subnavbar_link_color__']           = 'The font color for sub menu links';
+$lang['__subnavbar_link_hover_color__']     = 'The font color when the cursor hovers over sub menu links';
+$lang['__subnavbar_background_color__']     = 'The background color for the sub menu';
+$lang['__subnavbar_border_color__']         = 'The border color for the sub menu';
 $lang['__subnavbar_content_justify__']      = 'The content justification of the sub menu (can be flex-start, flex-end, center, space-between or space-around)';
 
-$lang['__tag_background_color__']           = 'The background color of tags (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__tag_text_color__']                 = 'The font color of tags  (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__tag_background_color__']           = 'The background color of tags';
+$lang['__tag_text_color__']                 = 'The font color of tags ';
 
-$lang['__breadcrumb_background_color__']    = 'The background color for the breadcrumb block (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__breadcrumb_font_size__']           = 'The font size for the breadcrumb block (can be any size unit: %, px, em, rem)';
-$lang['__breadcrumb_text_color__']          = 'The font color for breadcrumb text (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__breadcrumb_link_color__']          = 'The font color for breadcrumb links (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__breadcrumb_link_hover_color__']    = 'The font color when the cursor hovers over breadcrumb links (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__breadcrumb_background_color__']    = 'The background color for the breadcrumb block';
+$lang['__breadcrumb_font_size__']           = 'The font size for the breadcrumb block';
+$lang['__breadcrumb_text_color__']          = 'The font color for breadcrumb text';
+$lang['__breadcrumb_link_color__']          = 'The font color for breadcrumb links';
+$lang['__breadcrumb_link_hover_color__']    = 'The font color when the cursor hovers over breadcrumb links';
 
-$lang['__hero_title_color__']               = 'The font color for the hero title (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__hero_subtitle_color__']            = 'The font color for the hero sub title (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__hero_background_color__']          = 'The background color for the hero block (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__hero_title_color__']               = 'The font color for the hero title';
+$lang['__hero_subtitle_color__']            = 'The font color for the hero sub title';
+$lang['__hero_background_color__']          = 'The background color for the hero block';
 
-$lang['__sidebar_width__']                  = 'The sidebar width (can be any size unit: %, px, em, rem)';
-$lang['__sidebar_font_size__']              = 'The font size in sidebars (can be any size unit: %, px, em, rem)';
-$lang['__sidebar_text_color__']             = 'The font color for sidebars (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__sidebar_link_color__']             = 'The font color for links in sidebars (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__sidebar_link_hover_color__']       = 'The font color when the cursor hovers over links in sidebars (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__sidebar_background_color__']       = 'The background color for sidebars (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__sidebar_border_color__']           = 'The border color for sidebars (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__sidebar_width__']                  = 'The sidebar width';
+$lang['__sidebar_font_size__']              = 'The font size in sidebars';
+$lang['__sidebar_text_color__']             = 'The font color for sidebars';
+$lang['__sidebar_link_color__']             = 'The font color for links in sidebars';
+$lang['__sidebar_link_hover_color__']       = 'The font color when the cursor hovers over links in sidebars';
+$lang['__sidebar_background_color__']       = 'The background color for sidebars';
+$lang['__sidebar_border_color__']           = 'The border color for sidebars';
 
-$lang['__content_background__']             = 'The background color for page content (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__content_background__']             = 'The background color for page content';
 
-$lang['__toc_background_color__']           = 'The background color for the TOC (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__toc_border_color__']               = 'The border color for the TOC (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__toc_font_size__']                  = 'The font size for the TOC (can be any size unit: %, px, em, rem)';
-$lang['__toc_link_color__']                 = 'The font color for TOC links (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__toc_link_hover_color__']           = 'The font color when the cursor hovers over TOC links (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__toc_background_color__']           = 'The background color for the TOC';
+$lang['__toc_border_color__']               = 'The border color for the TOC';
+$lang['__toc_font_size__']                  = 'The font size for the TOC';
+$lang['__toc_link_color__']                 = 'The font color for TOC links';
+$lang['__toc_link_hover_color__']           = 'The font color when the cursor hovers over TOC links';
 
-$lang['__pagetools_color__']                = 'The color of page tool icons (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__pagetools_hover_color__']          = 'The color of page tools icons when cursor hovers (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__pagetools_color__']                = 'The color of page tool icons';
+$lang['__pagetools_hover_color__']          = 'The color of page tools icons when cursor hovers';
 
-$lang['__footer_font_size__']               = 'The font size for the footer (can be any size unit: %, px, em, rem)';
-$lang['__footer_text_color__']              = 'The font color for the footer (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__footer_link_color__']              = 'The font color for the footer links (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__footer_link_hover_color__']        = 'The font color when cursor hovers over footer links (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__footer_font_size__']               = 'The font size for the footer';
+$lang['__footer_text_color__']              = 'The font color for the footer';
+$lang['__footer_link_color__']              = 'The font color for the footer links';
+$lang['__footer_link_hover_color__']        = 'The font color when cursor hovers over footer links';
 $lang['__footer_text_align__']              = 'The text alignment for the footer (can be left, center, right or justify)';
-$lang['__footer_background_color__']        = 'The background color for the footer (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__footer_background_color__']        = 'The background color for the footer';
 
-$lang['__table_font_size__']                = 'The font size for tables (can be any size unit: %, px, em, rem)';
-$lang['__table_row_border_color__']         = 'The border color for tables (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__table_odd_row_color__']            = 'The background color for table odd rows (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__table_even_row_color__']           = 'The background color for table even rows (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__table_font_size__']                = 'The font size for tables';
+$lang['__table_row_border_color__']         = 'The border color for tables';
+$lang['__table_odd_row_color__']            = 'The background color for table odd rows';
+$lang['__table_even_row_color__']           = 'The background color for table even rows';
 
-$lang['__dropdown_color__']                 = 'The font color for dropdown menus (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__dropdown_hover_color__']           = 'The font color when the cursor hovers over dropdown menu items (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__dropdown_border_color__']          = 'The border color for dropdown menus (can be hexdecimal, RGB/RGBA or predefined browser color)';
-$lang['__dropdown_background_color__']      = 'The background color for dropdown menus (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__dropdown_color__']                 = 'The font color for dropdown menus';
+$lang['__dropdown_hover_color__']           = 'The font color when the cursor hovers over dropdown menu items';
+$lang['__dropdown_border_color__']          = 'The border color for dropdown menus';
+$lang['__dropdown_background_color__']      = 'The background color for dropdown menus';
 
-$lang['__section_edit_highlight__']         = 'The color when highlighting an edit area (can be hexdecimal, RGB/RGBA or predefined browser color)';
+$lang['__section_edit_highlight__']         = 'The color when highlighting an edit area';
 ?>
