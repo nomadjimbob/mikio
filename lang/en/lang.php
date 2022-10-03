1dbcd10a8d1342e83dc26b8ab6e9048ff719acc7
James Collins <james.collins@outlook.com.au>
2022-10-03 17:12:55 +1000

    cleanup and darkmode support

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -9,69 +9,80 @@
  */
 
  /* Theme */
-$lang['sidebar-title']                      = 'Sidebar';
-$lang['tools-menu']                         = 'Tools';
-$lang['home']                               = 'Home';
+$lang['sidebar-title']  = 'Sidebar';
+$lang['tools-menu']     = 'Tools';
+$lang['home']           = 'Home';
+
 
 /* Config */
-$lang['config_navbar']                      = 'Menubar';
-$lang['config_search']                      = 'Search';
-$lang['config_hero']                        = 'Hero';
-$lang['config_tags']                        = 'Tags';
-$lang['config_breadcrumb']                  = 'Breadcrumb';
-$lang['config_youarehere']                  = 'You Are Here';
-$lang['config_sidebar']                     = 'Sidebars';
-$lang['config_toc']                         = 'Table of Contents';
-$lang['config_pagetools']                   = 'Page Tools';
-$lang['config_footer']                      = 'Footer';
-$lang['config_license']                     = 'License';
-$lang['config_acl']                         = 'Permissions';
-$lang['config_sticky']                      = 'Sticky Parts';
+$lang['config_navbar']      = 'Menubar';
+$lang['config_search']      = 'Search';
+$lang['config_hero']        = 'Hero';
+$lang['config_tags']        = 'Tags';
+$lang['config_breadcrumb']  = 'Breadcrumb';
+$lang['config_youarehere']  = 'You Are Here';
+$lang['config_sidebar']     = 'Sidebars';
+$lang['config_toc']         = 'Table of Contents';
+$lang['config_pagetools']   = 'Page Tools';
+$lang['config_footer']      = 'Footer';
+$lang['config_license']     = 'License';
+$lang['config_acl']         = 'Permissions';
+$lang['config_sticky']      = 'Sticky Parts';
 
 /* Style.ini */
-$lang['__link__']                           = 'The font color of links';
-$lang['__line_height__']                    = 'The base font line height';
-$lang['__site_width__']                     = 'Width of the body content';
+$lang['__link__']       = 'The font color of links';
+$lang['__existing__']   = 'The font color of wiki links for existing pages';
+$lang['__missing__']    = 'The font color of wiki links for missing pages';
 
-$lang['__font_size__']                      = 'The base font size';
-$lang['__font_family__']                    = 'The base font family (fonts containing spaces must be in quotes. 
+$lang['__line_height__']    = 'The base font line height';
+$lang['__site_width__']     = 'Width of the body content';
+
+$lang['__font_size__']              = 'The base font size';
+$lang['__font_family__']            = 'The base font family (fonts containing spaces must be in quotes. Multiple fonts 
+can be seperated with commas)';
+$lang['__admin_background_color__'] = 'Background color of admin pages';
+
+$lang['__code_font_family__']       = 'The font family for code blocks (fonts containing spaces must be in quotes. 
 Multiple fonts can be seperated with commas)';
-$lang['__admin_background_color__']         = 'Background color of admin pages';
+$lang['__code_font_size__']         = 'The font size for code blocks';
+$lang['__code_text_color__']        = 'The font color for code blocks';
+$lang['__code_background_color__']  = 'The background color for code blocks';
+$lang['__code_line_height__']       = 'The font line height for code blocks';
+$lang['__code_word_wrap__']         = 'The word wrap style for code blocks';
+$lang['__code_border_color__']      = 'The border color for code blocks';
 
-$lang['__code_font_family__']               = 'The font family for code blocks (fonts containing spaces must be in 
-quotes. Multiple fonts can be seperated with commas)';
-$lang['__code_font_size__']                 = 'The font size for code blocks';
-$lang['__code_text_color__']                = 'The font color for code blocks';
-$lang['__code_background_color__']          = 'The background color for code blocks';
-$lang['__code_line_height__']               = 'The font line height for code blocks';
-$lang['__code_word_wrap__']                 = 'The word wrap style for code blocks';
-$lang['__code_border_color__']              = 'The border color for code blocks';
+$lang['__control_text_color__']                     = 'The font color for controls';
+$lang['__control_border_color__']                   = 'The border color for controls';
+$lang['__control_background_color__']               = 'The background color for controls';
+$lang['__input_border_color__']                     = 'The border color for controls';
+$lang['__button_default_text_color__']              = 'The font color for the default button';
+$lang['__button_default_background_color__']        = 'The background color for the default button';
+$lang['__button_default_border_color__']            = 'The border color for the default button';
+$lang['__button_default_text_hover_color__']        = 'The font color for the default button when cursor hovers over';
+$lang['__button_default_background_hover_color__']  = 'The background color for the default button when cursor hovers 
+over';
+$lang['__button_default_border_hover_color__']      = 'The border color for the default button when cursor hovers over';
 
-$lang['__control_text_color__']             = 'The font color for controls';
-$lang['__control_border_color__']           = 'The border color for controls';
-$lang['__control_background_color__']       = 'The background color for controls';
-$lang['__input_border_color__']             = 'The border color for controls';
+$lang['__topheader_background_color__'] = 'The background color for the top header part';
+$lang['__header_background_color__']    = 'The background color for the header part';
 
-$lang['__topheader_background_color__']     = 'The background color for the top header part';
-$lang['__header_background_color__']        = 'The background color for the header part';
-
-$lang['__navbar_background_color__']        = 'The background color for the menu';
-$lang['__navbar_border_color__']            = 'The border color for the menu';
-$lang['__navbar_brand_text_color__']        = 'The font color for the site title';
-$lang['__navbar_brand_tagline_color__']     = 'The font color for the site tagline';
-$lang['__navbar_content_justify__']         = 'The content justification of the site menu (can be flex-start, flex-end, 
+$lang['__navbar_background_color__']    = 'The background color for the menu';
+$lang['__navbar_border_color__']        = 'The border color for the menu';
+$lang['__navbar_brand_text_color__']    = 'The font color for the site title';
+$lang['__navbar_brand_tagline_color__'] = 'The font color for the site tagline';
+$lang['__navbar_content_justify__']     = 'The content justification of the site menu (can be flex-start, flex-end, 
 center, space-between or space-around)';
 
-$lang['__subnavbar_text_color__']           = 'The font color for sub menu text';
-$lang['__subnavbar_link_color__']           = 'The font color for sub menu links';
-$lang['__subnavbar_link_hover_color__']     = 'The font color when the cursor hovers over sub menu links';
-$lang['__subnavbar_background_color__']     = 'The background color for the sub menu';
-$lang['__subnavbar_border_color__']         = 'The border color for the sub menu';
-$lang['__subnavbar_content_justify__']      = 'The content justification of the sub menu (can be flex-start, flex-end, 
+$lang['__subnavbar_text_color__']       = 'The font color for sub menu text';
+$lang['__subnavbar_link_color__']       = 'The font color for sub menu links';
+$lang['__subnavbar_link_hover_color__'] = 'The font color when the cursor hovers over sub menu links';
+$lang['__subnavbar_background_color__'] = 'The background color for the sub menu';
+$lang['__subnavbar_border_color__']     = 'The border color for the sub menu';
+$lang['__subnavbar_content_justify__']  = 'The content justification of the sub menu (can be flex-start, flex-end, 
 center, space-between or space-around)';
 
-$lang['__tag_background_color__']           = 'The background color of tags';
-$lang['__tag_text_color__']                 = 'The font color of tags ';
+$lang['__tag_background_color__']   = 'The background color of tags';
+$lang['__tag_text_color__']         = 'The font color of tags ';
 
 $lang['__breadcrumb_background_color__']    = 'The background color for the breadcrumb block';
 $lang['__breadcrumb_font_size__']           = 'The font size for the breadcrumb block';
@@ -79,47 +90,54 @@ $lang['__breadcrumb_text_color__']          = 'The font color for breadcrumb tex
 $lang['__breadcrumb_link_color__']          = 'The font color for breadcrumb links';
 $lang['__breadcrumb_link_hover_color__']    = 'The font color when the cursor hovers over breadcrumb links';
 
-$lang['__hero_title_color__']               = 'The font color for the hero title';
-$lang['__hero_subtitle_color__']            = 'The font color for the hero sub title';
-$lang['__hero_background_color__']          = 'The background color for the hero block';
+$lang['__hero_title_color__']       = 'The font color for the hero title';
+$lang['__hero_subtitle_color__']    = 'The font color for the hero sub title';
+$lang['__hero_background_color__']  = 'The background color for the hero block';
 
-$lang['__sidebar_width__']                  = 'The sidebar width';
-$lang['__sidebar_font_size__']              = 'The font size in sidebars';
-$lang['__sidebar_text_color__']             = 'The font color for sidebars';
-$lang['__sidebar_link_color__']             = 'The font color for links in sidebars';
-$lang['__sidebar_link_hover_color__']       = 'The font color when the cursor hovers over links in sidebars';
-$lang['__sidebar_background_color__']       = 'The background color for sidebars';
-$lang['__sidebar_border_color__']           = 'The border color for sidebars';
+$lang['__sidebar_width__']              = 'The sidebar width';
+$lang['__sidebar_font_size__']          = 'The font size in sidebars';
+$lang['__sidebar_text_color__']         = 'The font color for sidebars';
+$lang['__sidebar_link_color__']         = 'The font color for links in sidebars';
+$lang['__sidebar_link_hover_color__']   = 'The font color when the cursor hovers over links in sidebars';
+$lang['__sidebar_background_color__']   = 'The background color for sidebars';
+$lang['__sidebar_border_color__']       = 'The border color for sidebars';
 
-$lang['__content_background__']             = 'The background color for page content';
+$lang['__content_background__'] = 'The background color for page content';
 
-$lang['__toc_background_color__']           = 'The background color for the TOC';
-$lang['__toc_border_color__']               = 'The border color for the TOC';
-$lang['__toc_font_size__']                  = 'The font size for the TOC';
-$lang['__toc_link_color__']                 = 'The font color for TOC links';
-$lang['__toc_link_hover_color__']           = 'The font color when the cursor hovers over TOC links';
+$lang['__toc_background_color__']   = 'The background color for the TOC';
+$lang['__toc_border_color__']       = 'The border color for the TOC';
+$lang['__toc_font_size__']          = 'The font size for the TOC';
+$lang['__toc_link_color__']         = 'The font color for TOC links';
+$lang['__toc_link_hover_color__']   = 'The font color when the cursor hovers over TOC links';
 
-$lang['__pagetools_color__']                = 'The color of page tool icons';
-$lang['__pagetools_hover_color__']          = 'The color of page tools icons when cursor hovers';
+$lang['__pagetools_color__']        = 'The color of page tool icons';
+$lang['__pagetools_hover_color__']  = 'The color of page tools icons when cursor hovers';
 
-$lang['__footer_font_size__']               = 'The font size for the footer';
-$lang['__footer_text_color__']              = 'The font color for the footer';
-$lang['__footer_link_color__']              = 'The font color for the footer links';
-$lang['__footer_link_hover_color__']        = 'The font color when cursor hovers over footer links';
-$lang['__footer_text_align__']              = 'The text alignment for the footer (can be left, center, right or 
-justify)';
-$lang['__footer_background_color__']        = 'The background color for the footer';
+$lang['__footer_font_size__']           = 'The font size for the footer';
+$lang['__footer_text_color__']          = 'The font color for the footer';
+$lang['__footer_link_color__']          = 'The font color for the footer links';
+$lang['__footer_link_hover_color__']    = 'The font color when cursor hovers over footer links';
+$lang['__footer_text_align__']          = 'The text alignment for the footer (can be left, center, right or justify)';
+$lang['__footer_background_color__']    = 'The background color for the footer';
 
-$lang['__table_font_size__']                = 'The font size for tables';
-$lang['__table_row_border_color__']         = 'The border color for tables';
-$lang['__table_odd_row_color__']            = 'The font color for table odd rows';
-$lang['__table_odd_row_background_color__'] = 'The background color for table odd rows';
-$lang['__table_even_row_color__']           = 'The font color for table even rows';
+$lang['__table_font_size__']                    = 'The font size for tables';
+$lang['__table_row_border_color__']             = 'The border color for tables';
+$lang['__table_odd_row_color__']                = 'The font color for table odd rows';
+$lang['__table_odd_row_background_color__']     = 'The background color for table odd rows';
+$lang['__table_even_row_color__']               = 'The font color for table even rows';
 $lang['__table_even_row_background_color__']    = 'The background color for table odd rows';
 
-$lang['__dropdown_color__']                 = 'The font color for dropdown menus';
-$lang['__dropdown_hover_color__']           = 'The font color when the cursor hovers over dropdown menu items';
-$lang['__dropdown_border_color__']          = 'The border color for dropdown menus';
-$lang['__dropdown_background_color__']      = 'The background color for dropdown menus';
+$lang['__dropdown_color__']             = 'The font color for dropdown menus';
+$lang['__dropdown_hover_color__']       = 'The font color when the cursor hovers over dropdown menu items';
+$lang['__dropdown_border_color__']      = 'The border color for dropdown menus';
+$lang['__dropdown_background_color__']  = 'The background color for dropdown menus';
 
-$lang['__section_edit_highlight__']         = 'The color when highlighting an edit area';
+$lang['__section_edit_highlight__']     = 'The color when highlighting an edit area';
+
+/* Add darkmode language */
+$lang['__darkmode_text__']          = 'Main text color';
+$lang['__darkmode_background__']    = 'Main background color';
+
+foreach ($lang as $key => $val) {
+    $lang['__darkmode_' . substr($key, 2)] = $val;
+}
