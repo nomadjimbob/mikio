9adaf947ddd56d7dd7b28de0e4ac3cc56d6d18ac
James Collins <james.collins@outlook.com.au>
2023-12-12 19:01:33 +1000

    formatting

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 /* FastWiki Plugin Support */
-function tpl_fastwiki_support() {}
+function tpl_fastwiki_support() { }
 
-jQuery(window).on('fastwiki:afterSwitch', function(evt, viewMode, isSectionEdit, prevViewMode) {
+jQuery(window).on('fastwiki:afterSwitch', function (evt, viewMode, isSectionEdit, prevViewMode) {
     if (viewMode === 'show') {
         // Check if the source element 'article.mikio-article div.mikio-article-content.content_initial #dw__toc' exists
         var sourceToc = jQuery('article.mikio-article div.mikio-article-content.content_initial #dw__toc');
-        
+
         if (sourceToc.length > 0) {
             // Check if the target location 'article.mikio-article .mikio-toc #dw__toc' already has a 'dw__toc' element
             var targetToc = jQuery('article.mikio-article .mikio-toc #dw__toc');
-            
+
             if (targetToc.length > 0) {
                 // If the target already has a 'dw__toc', remove it
                 targetToc.remove();
             }
-            
+
             // Move the 'dw__toc' from the source to the target location
             sourceToc.appendTo('article.mikio-article .mikio-toc');
 
