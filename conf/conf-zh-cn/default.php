a5bf1727ba7a512609436828797cf90bd25b12fa
Little-data <53138165+Little-Data@users.noreply.github.com>
2024-06-20 16:12:57 +0800

    Create Chinese Simplified default.php

---
+++ conf/conf-zh-cn/default.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * DokuWiki Mikio Template Default Configuration(Chinese Simplified)
+ *
+ * @link    http://dokuwiki.org/template:mikio
+ * @author  Little-Data <https://github.com/Little-Data>
+ * @license GPLv2 (http://www.gnu.org/licenses/gpl-2.0.html)
+ */
+
+$conf['iconTag']                = 'icon';
+$conf['customTheme']            = '';
+$conf['showNotifications']      = '总是';
+$conf['useLESS']                = 1;
+$conf['brandURLGuest']          = '';
+$conf['brandURLUser']           = '';
+$conf['showLightDark']          = 0;
+$conf['autoLightDark']          = 0;
+
+$conf['navbarUseTitleIcon']     = 1;
+$conf['navbarTitleIconHeight']  = '';
+$conf['navbarTitleIconWidth']   = '';
+$conf['navbarUseTitleText']     = 1;
+$conf['navbarUseTaglineText']   = 1;
+$conf['navbarCustomMenuText']   = '';
+
+$conf['navbarDWMenuType']       = '图标';
+$conf['navbarDWMenuCombine']    = '下拉式';
+
+$conf['navbarPosLeft']          = '自定义';
+$conf['navbarPosMiddle']        = '搜索';
+$conf['navbarPosRight']         = 'dokuwiki';
+$conf['navbarShowSub']          = 0;
+
+$conf['navbarItemShowCreate']   = '总是';
+$conf['navbarItemShowShow']     = '总是';
+$conf['navbarItemShowRevs']     = '总是';
+$conf['navbarItemShowBacklink'] = '总是';
+$conf['navbarItemShowRecent']   = '总是';
+$conf['navbarItemShowMedia']    = '总是';
+$conf['navbarItemShowIndex']    = '总是';
+$conf['navbarItemShowProfile']  = '总是';
+$conf['navbarItemShowAdmin']    = '总是';
+$conf['navbarItemShowLogin']    = '总是';
+$conf['navbarItemShowLogout']   = '总是';
+
+$conf['searchButton']           = '图标';
+$conf['searchUseTypeahead']     = 1;
+
+$conf['heroTitle']              = 1;
+$conf['heroImagePropagation']   = 1;
+
+$conf['tagsConsolidate']        = 1;
+$conf['tagsShowHero']           = 1;
+
+$conf['breadcrumbHideHome']     = 1;
+$conf['breadcrumbPosition']     = '页面大标题';
+$conf['breadcrumbPrefix']       = 0;
+$conf['breadcrumbPrefixText']   = '路径：';
+$conf['breadcrumbSep']          = 0;
+$conf['breadcrumbSepText']      = ' » ';
+
+$conf['youarehereHideHome']     = 1;
+$conf['youareherePosition']     = '页面大标题';
+$conf['youareherePrefix']       = 0;
+$conf['youareherePrefixText']   = '您在这里：';
+$conf['youarehereSep']          = 0;
+$conf['youarehereSepText']      = ' » ';
+$conf['youarehereHome']         = '页面标题';
+$conf['youarehereShowLast']     = 0;
+
+$conf['sidebarShowLeft']        = 1;
+$conf['sidebarAlwaysShowLeft']  = 0;
+$conf['sidebarLeftRow1']        = '已登录用户';
+$conf['sidebarLeftRow2']        = '搜索';
+$conf['sidebarLeftRow3']        = '内容';
+$conf['sidebarLeftRow4']        = '无';
+$conf['sidebarMobileDefaultCollapse']    = 1;
+$conf['sidebarShowRight']       = 1;
+$conf['sidebarAlwaysShowright'] = 0;
+
+$conf['tocFull']                = 0;
+
+$conf['pageToolsFloating']      = '总是';
+$conf['pageToolsFooter']        = '无';
+
+$conf['pageToolsShowCreate']    = '总是';
+$conf['pageToolsShowEdit']      = '总是';
+$conf['pageToolsShowRevs']      = '总是';
+$conf['pageToolsShowBacklink']  = '总是';
+$conf['pageToolsShowTop']       = '总是';
+
+$conf['footerPageInfoText']     = '{file} · %lastmod% {date}[LOGGEDIN=[USER= %by% {user}][LOCKED= · %lockedby% {locked}]]';
+$conf['footerCustomMenuText']   = '';
+$conf['footerSearch']           = 0;
+$conf['footerInPage']           = 0;
+
+$conf['licenseType']            = '徽章';
+$conf['licenseImageOnly']       = 0;
+
+$conf['includePageUseACL']      = 1;
+$conf['includePagePropagate']   = 1;
+
+$meta['stickyTopHeader']        = 0;
+$meta['stickyNavbar']           = 0;
+$meta['stickyHeader']           = 0;
+$meta['stickyLeftSidebar']      = 0;
